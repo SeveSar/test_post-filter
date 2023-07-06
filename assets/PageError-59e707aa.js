61e4d870ff65aa95d9856166d8e09a33746fdab8
Averin Artem <averin.dev@mail.ru>
2023-07-06 22:12:36 +0500

    Initial dist subtree commit

---
+++ assets/PageError-59e707aa.js
@@ -0,0 +1,1 @@
+import{_ as c,o,c as a,p as s,m as _,a as t}from"./index-c1a2f86c.js";const r={},d=e=>(s("data-v-ccab6397"),e=e(),_(),e),n={class:"page-error"},p=d(()=>t("h1",{class:"page-error__title"},"Page not found(404)",-1)),i=[p];function l(e,f){return o(),a("div",n,i)}const u=c(r,[["render",l],["__scopeId","data-v-ccab6397"]]);export{u as default};
